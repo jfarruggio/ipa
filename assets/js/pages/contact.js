39619a9667656b23903155e0f1e232d2de84b884
Joseph Farruggio <Jfarruggio@abledistributors.com>
2014-04-06 15:48:25 -0500

    Contact form added and reposition of contact page

--- assets/js/pages/contact.js
+++ assets/js/pages/contact.js
@@ -8,12 +8,12 @@ var Contact = function () {
 			$(document).ready(function(){
 			  map = new GMaps({
 				div: '#map',
-				lat: 40.748866,
-				lng: -73.988366
+				lat: 43.175365,
+				lng: -89.219985,
 			  });
 			   var marker = map.addMarker({
-					lat: 40.748866,
-					lng: -73.988366,
+					lat: 43.175365,
+					lng: -89.219985,
 		            title: 'Loop, Inc.'
 		        });
 			});
